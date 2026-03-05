6c81c5f90f77d228fd76ffc5a2b3ff9891d568dc
Elisei Morosan <eliseimorosan@gmail.com>
2026-03-05 09:21:57 +0200

    check

--- sistem-spital/src/App.vue
+++ sistem-spital/src/App.vue
@@ -4,9 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+    <p>Date sau Da-te</p>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
