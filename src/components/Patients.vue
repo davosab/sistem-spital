6b593ffc1d5c174e869ef16df1625bc168c6bcc3
davosab <davidsabau72@gmail.com>
2026-03-05 11:45:17 +0200

    button component

--- src/components/Patients.vue
+++ src/components/Patients.vue
@@ -1,7 +1,11 @@
-<template> 
-<div class="ml-[200px] h-full" >
+<script setup>
+import Button from "./Button.vue";
+</script>
+
+<template>
+  <div class="ml-[200px] h-full">
     <div class="flex justify-self-end">
-        <button class=" mr-[50px] mt-[25px] text-[16px] font-semibold px-[15px] py-[10px] bg-[#56D788] rounded-[10px]"> Add new patient </button>
+      <Button text="Add new patient" />
     </div>
-</div>
+  </div>
 </template>
