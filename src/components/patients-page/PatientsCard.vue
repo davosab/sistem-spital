a1f1ca0ccf913b249d645578c82d09c340d0d77f
Elisei Morosan <eliseimorosan@gmail.com>
2026-03-05 11:43:37 +0200

    Structured files, also added patients page design

---
+++ src/components/patients-page/PatientsCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="patients-card" class="w-[full] bg-white mx-[30px] mb-[30px] rounded-[10px]">
+        <h1 class="text-[18px] font-semibold mt-3">
+            Patients
+        </h1>
+
+        <div class="grid grid-cols-1 mt-2 max-h-[800px] overflow-y-auto ">
+            <div class="grid grid-cols-9 border-b border-t border-gray-300 p-3 text-center items-center">
+                <p class="text-[16px] font-semibold">
+                    ID
+                </p>
+                <p class="text-[16px] font-semibold">
+                    First Name
+                </p>
+                <p class="text-[16px] font-semibold">
+                    Last Name
+                </p>
+                <p class="text-[16px] font-semibold">
+                    CNP
+                </p>
+                 <p class="text-[16px] font-semibold">
+                    Date of Birth
+                </p>
+                 <p class="text-[16px] font-semibold">
+                    Gender
+                </p>
+                <p class="text-[16px] font-semibold">
+                    Status
+                </p>
+
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/Patients.vue
+++ src/pages/Patients.vue
@@ -1,7 +1,17 @@
+<script setup>
+import PatientsCard from '../components/patients-page/PatientsCard.vue';
+</script>
 <template> 
 <div class="ml-[200px] h-full" >
     <div class="flex justify-self-end">
         <button class=" mr-[50px] mt-[25px] text-[16px] font-semibold px-[15px] py-[10px] bg-[#56D788] rounded-[10px]"> Add new patient </button>
     </div>
+
+    <div class="w-full h-full">
+        <PatientsCard />
+    </div>
 </div>
+
+
+
 </template>
