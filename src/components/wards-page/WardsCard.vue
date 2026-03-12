2404cba803bffc7df90b193eacc0d189e4d8faa3
Elisei Morosan <eliseimorosan@gmail.com>
2026-03-12 11:55:08 +0200

    finished shwoing all results

--- src/components/wards-page/WardsCard.vue
+++ src/components/wards-page/WardsCard.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { supabase } from "/src/lib/supabaseClient";
+
+// array that will contain objects with database records
+const wards = ref([]);
+
+// querry from supabase database
+async function getWards() {
+    const { data } = await supabase.from("wards").select("*");
+    wards.value = data;
+}
+onMounted(() => {
+    getWards();
+});
+</script>
+
 <template>
     <div id="patients-card" class="w-[full] bg-white mx-[30px] mb-[30px] rounded-[10px]">
         <h1 class="text-[18px] font-semibold mt-3">
@@ -27,8 +44,32 @@
                 <p class="text-[16px] font-semibold">
                     Created At
                 </p>
+                <p class="text-[16px] font-semibold">Edit</p>
+                <p class="text-[16px] font-semibold">Delete</p>
 
             </div>
+             <ul>
+                <li v-for="(ward, index) in wards" :key="ward.id"
+                    class="grid grid-cols-9 border-b border-gray-200 p-3 text-center items-center hover:bg-gray-50">
+                    <p class="text-[14px]">{{ index + 1 }}</p>
+                    <p class="text-[14px]">{{ ward.name }}</p>
+                    <p class="text-[14px]">{{ ward.type }}</p>
+                    <p class="text-[14px]">{{ ward.capacity }}</p>
+                    <p class="text-[14px]">{{ ward.occupied }}</p>
+                    <p class="text-[14px]">{{ ward.floor }}</p>
+                    <p class="text-[14px]">
+                        {{ new Date(ward.created_at).toLocaleDateString() }}
+                    </p>
+                    <button
+                        class="mr-[10px] bg-[#56D788] hover:bg-[#2cba62] text-[12px] font-medium px-2 py-1 rounded-md transition-colors duration-200">
+                        Edit
+                    </button>
+                    <button
+                        class="ml-[10px] bg-red-500 hover:bg-red-600 text-white text-[12px] font-medium px-3 py-1 rounded-md transition-colors duration-200">
+                        Delete
+                    </button>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
