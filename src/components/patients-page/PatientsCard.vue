b53f82109f0e648c93241faa8d571e1b59099494
davosab <davidsabau72@gmail.com>
2026-03-19 09:13:01 +0200

    changed patient id to uuid

--- src/components/patients-page/PatientsCard.vue
+++ src/components/patients-page/PatientsCard.vue
@@ -7,71 +7,71 @@ const patients = ref([]);
 
 // querry from supabase database
 async function getPatients() {
-    const { data } = await supabase.from("patients").select("*");
-    patients.value = data;
+  const { data } = await supabase.from("patients").select("*");
+  patients.value = data;
 }
 
 async function deletePatient(id) {
-    const { error } = await supabase.from("patients").delete().eq("id", id);
-    if (error) {
-        console.error(error.message);
-    } else {
-        patients.value = patients.value.filter((patient) => patient.id !== id);
-    }
+  const { error } = await supabase.from("patients").delete().eq("id", id);
+  if (error) {
+    console.error(error.message);
+  } else {
+    patients.value = patients.value.filter((patient) => patient.id !== id);
+  }
 }
 
 onMounted(() => {
-    getPatients();
+  getPatients();
 });
 </script>
 
 <template>
-    <div
-        id="patients-card"
-        class="w-[full] bg-white mx-[30px] mb-[30px] rounded-[10px]"
-    >
-        <h1 class="text-[18px] font-semibold mt-3">Patients</h1>
+  <div
+    id="patients-card"
+    class="w-[full] bg-white mx-[30px] mb-[30px] rounded-[10px]"
+  >
+    <h1 class="text-[18px] font-semibold mt-3">Patients</h1>
 
-        <div class="grid grid-cols-1 mt-2 max-h-[800px] overflow-y-auto">
-            <div
-                class="grid grid-cols-9 border-b border-t border-gray-300 p-3 text-center items-center"
-            >
-                <p class="text-[16px] font-semibold">ID</p>
-                <p class="text-[16px] font-semibold">First Name</p>
-                <p class="text-[16px] font-semibold">Last Name</p>
-                <p class="text-[16px] font-semibold">CNP</p>
-                <p class="text-[16px] font-semibold">Date of Birth</p>
-                <p class="text-[16px] font-semibold">Gender</p>
-                <p class="text-[16px] font-semibold">Status</p>
-                <p class="text-[16px] font-semibold">Edit</p>
-                <p class="text-[16px] font-semibold">Delete</p>
-            </div>
-            <ul>
-                <li
-                    v-for="(patient, index) in patients"
-                    :key="patient.id"
-                    class="grid grid-cols-9 border-b border-gray-200 p-3 text-center items-center hover:bg-gray-50"
-                >
-                    <p class="text-[14px]">{{ index + 1 }}</p>
-                    <p class="text-[14px]">{{ patient.first_name }}</p>
-                    <p class="text-[14px]">{{ patient.last_name }}</p>
-                    <p class="text-[14px]">{{ patient.cnp }}</p>
-                    <p class="text-[14px]">{{ patient.date_of_birth }}</p>
-                    <p class="text-[14px]">{{ patient.gender }}</p>
-                    <p class="text-[14px]">{{ patient.status }}</p>
-                    <button
-                        class="mr-[10px] bg-[#56D788] hover:bg-[#2cba62] text-[12px] font-medium px-2 py-1 rounded-md transition-colors duration-200"
-                    >
-                        Edit
-                    </button>
-                    <button
-                        @click="deletePatient(patient.id)"
-                        class="ml-[10px] bg-red-500 hover:bg-red-600 text-white text-[12px] font-medium px-3 py-1 rounded-md transition-colors duration-200"
-                    >
-                        Delete
-                    </button>
-                </li>
-            </ul>
-        </div>
+    <div class="grid grid-cols-1 mt-2 max-h-[800px] overflow-y-auto">
+      <div
+        class="grid grid-cols-9 border-b border-t border-gray-300 p-3 text-center items-center"
+      >
+        <p class="text-[16px] font-semibold">ID</p>
+        <p class="text-[16px] font-semibold">First Name</p>
+        <p class="text-[16px] font-semibold">Last Name</p>
+        <p class="text-[16px] font-semibold">CNP</p>
+        <p class="text-[16px] font-semibold">Date of Birth</p>
+        <p class="text-[16px] font-semibold">Gender</p>
+        <p class="text-[16px] font-semibold">Status</p>
+        <p class="text-[16px] font-semibold">Edit</p>
+        <p class="text-[16px] font-semibold">Delete</p>
+      </div>
+      <ul>
+        <li
+          v-for="(patient, index) in patients"
+          :key="patient.id"
+          class="grid grid-cols-9 border-b border-gray-200 p-3 text-center items-center hover:bg-gray-50"
+        >
+          <p class="text-[14px]">{{ patient.id }}</p>
+          <p class="text-[14px]">{{ patient.first_name }}</p>
+          <p class="text-[14px]">{{ patient.last_name }}</p>
+          <p class="text-[14px]">{{ patient.cnp }}</p>
+          <p class="text-[14px]">{{ patient.date_of_birth }}</p>
+          <p class="text-[14px]">{{ patient.gender }}</p>
+          <p class="text-[14px]">{{ patient.status }}</p>
+          <button
+            class="mr-[10px] bg-[#56D788] hover:bg-[#2cba62] text-[12px] font-medium px-2 py-1 rounded-md transition-colors duration-200"
+          >
+            Edit
+          </button>
+          <button
+            @click="deletePatient(patient.id)"
+            class="ml-[10px] bg-red-500 hover:bg-red-600 text-white text-[12px] font-medium px-3 py-1 rounded-md transition-colors duration-200"
+          >
+            Delete
+          </button>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
